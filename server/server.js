76459b013921586125d3ee5b4c256308f92db199
Michal <viniarski@gmail.com>
2024-02-01 15:28:51 +0000

    add root db get post delete seed.js

--- server/server.js
+++ server/server.js
@@ -1,25 +1,55 @@
-import express from "express";
-import cors from "cors";
-import Database from "better-sqlite3";
+import express from 'express';
+import cors from 'cors';
+import Database from 'better-sqlite3';
 
-const PORT = "7070";
+const PORT = '7070';
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-app.listen(PORT, () => {
-  console.log(`Server started on PORT ${PORT}`);
+// database
+const db = new Database('database.db')
+  .prepare(
+    'CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, messages TEXT)'
+  )
+  .run();
+
+// root route
+app.get("/", function (req, res) {
+  res.json('Root route!');
 });
 
-app.post("/messages", (req, res) => {
+// get
+app.get('/messages', (req, res) => {
   try {
-  const username = req.body.username;
-  const message = req.body.message;
-
-  const newMessage = db.prepare(`INSERT INTO messages (username, message) VALUES (?, ?)`).run(username, message)
-  res.status(200).json(newMessage)
+      if (req.query.id) {
+          let message = db.prepare(`SELECT * FROM messages WHERE id = ?`).all(req.query.id)
+          res.status(200).json(message)
+          return 
+      }
+      let messages = db.prepare(`SELECT * FROM messages`).all()
+      res.status(200).json(messages)
   } catch (err) {
-    res.status(500).json({error : err})
+      res.status(500).json(err)
   }
+})
+
+// post
+app.post('/messages', (req, res) => {
+  try {
+    const username = req.body.username;
+    const message = req.body.message;
+
+    const newMessage = db
+      .prepare(`INSERT INTO messages (username, message) VALUES (?, ?)`)
+      .run(username, message);
+    res.status(200).json(newMessage);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
 });
